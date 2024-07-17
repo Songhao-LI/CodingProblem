29a38f48329c7627f57665b4b4f80b301d8c9d3a
Songhao-LI <145402008+Songhao-LI@users.noreply.github.com>
2024-07-17 11:01:45 -0400

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,27 @@
+def problem1(source, target):
+    res = 0
+    
+    source_arr = deque(source)
+    target_arr = deque(target)
+    n = len(source_arr)
+    m = len(target_arr)
+    
+    print(source_arr)
+    print(target_arr)
+    
+    while target_arr:
+        for i in range(n):
+            if source_arr[i] == target_arr[0]:
+                target_arr.popleft()
+        if len(target_arr) == m:
+            return -1
+        else:
+            m = len(target_arr)
+            res += 1
+
+    return res
+
+
+print(problem1("abc", "abcbc"))
+print(problem1("abc", "acdbc"))
+print(problem1("xyz", "xzyxz"))
